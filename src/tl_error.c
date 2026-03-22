0c41809c192662a0a972083c6716f6a2d2ce0210
Fatih Cetinkaya <965295+devfacet@users.noreply.github.com>
2026-03-21 21:53:42 -0400

    Revert tl_error.c

--- src/tl_error.c
+++ src/tl_error.c
@@ -24,26 +24,21 @@ void tl_error_set(TLError *error, TLErrorCode code, const char *message, ...) {
     // Set message if provided
     if (message) {
         va_list args;
-        va_list args_copy;
-
         va_start(args, message);
-        va_copy(args_copy, args);
-
-        int size = vsnprintf(NULL, 0, message, args_copy);
-        va_end(args_copy);
+        int size = vsnprintf(NULL, 0, message, args);
+        va_end(args);
 
         if (size < 0) {
-            va_end(args);
             return; // ignore if vsnprintf fails
         }
 
-        error->message_size = (size_t)size + 1;
-        error->message = malloc(error->message_size); // include null terminator by adding 1
+        error->message = malloc(size + 1); // include null terminator by adding 1
         if (!error->message) {
-            va_end(args);
             return; // ignore if malloc fails
         }
+        error->message_size = size + 1;
 
+        va_start(args, message);
         vsnprintf((char *)error->message, error->message_size, message, args);
         va_end(args);
     }
